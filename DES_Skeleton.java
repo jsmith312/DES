2555b256b55e258baf09b2ba07f4f08dc1dd157e
jsmith312 <jts312@msn.com>
2015-10-23 18:16:01 -0700

    permute and split key

--- DES_Skeleton.java
+++ DES_Skeleton.java
@@ -11,7 +11,8 @@ import gnu.getopt.Getopt;
 public class DES_Skeleton {
     private int K_BITS = 64;
     private BitSet K_BITSET;
-    private int K+_BITS = 56;
+    private int Kp_BITS = 56;
+    private BitSet Kp_BITSET;
     private int R = 56;
     
 	public static void main(String[] args) {
@@ -90,15 +91,60 @@ public class DES_Skeleton {
 	}
 
 
-	static void genDESkey(){
+    static void genDESkey(){
         SecureRandom rnd = new SecureRandom();
-        BitSet bs = new BitSet(K_BITS);
+        K_BITSET = new BitSet(64);
         for(int i = 0; i < K_BITS; i++) {
-            bs.set(i, rnd.nextBoolean());
+            K_BITSET.set(i, rnd.nextBoolean());
         }
-        K_BITSET = bs;
-	}
-
+        System.out.println("K: ");
+        System.out.println(K_BITSET.length());
+        printAsBinary(K_BITSET);
+    }
+    
+    static void permute56bits() {
+        Kp_BITSET = new BitSet(56);
+        for (int i = 0; i < SBoxes.PC1.length; i++) {
+            Kp_BITSET.set(i, K_BITSET.get(SBoxes.PC1[i]));
+        }
+        System.out.println("K+: ");
+        System.out.println(Kp_BITSET.length());
+        printAsBinary(Kp_BITSET);
+    }
+    
+    static void genKeys() {
+        BitSet left = new BitSet(28);
+        BitSet right = new BitSet(28);
+        int rightSide = 0;
+        
+        // set left & right bits
+        for (int i = 0; i < 28; i++) {
+            left.set(i, Kp_BITSET.get(i));
+        }
+        int index = 28;
+        for (int k = 0; k < 28; k++) {
+            right.set(k, Kp_BITSET.get(index+k));
+        }
+        
+        System.out.print("C-0: ");
+        printAsBinary(left);
+        System.out.println();
+        System.out.println(left.length());
+        System.out.print("D-0: \n");
+        System.out.println(right.length());
+        printAsBinary(right);
+    }
+    
+    static void printAsBinary(BitSet bs) {
+        for (int i = 0; i < bs.length(); i++) {
+            if (bs.get(i)) { // if true, 1
+                System.out.print(1);
+            } else { // else is 0
+                System.out.print(0);
+            }
+        }
+        System.out.println();
+    }
 
 	/**
 	 * This function Processes the Command Line Arguments.
